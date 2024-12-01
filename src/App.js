48528ce3a0ef9692f1848291589f068d0ae332f0
Doina <leovchind@gmail.com>
2024-12-01 20:37:12 +0200

    Set up routing and added Navbar, Footer, and page routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+// Import components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// Import pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+import "./index.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navbar Component */}
+        <Navbar />
+
+        {/* Routes for different pages */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/skills" component={Skills} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+
+        {/* Footer Component */}
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
